ffc53cd1339eb238854956414c590fd75aed9a80
ポッピコーン <43871609+popiicorn@users.noreply.github.com>
2023-01-31 23:45:32 +0900

    ステージ10まで実装

--- Assets/Desgin/Script/SageInstantiate.cs
+++ Assets/Desgin/Script/SageInstantiate.cs
@@ -13,7 +13,6 @@ public class SageInstantiate : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //Instantiate(stageprefab[1]);
 
         //ポイント値のロード
         clearpoint = ES3.Load("Clearkey", clearpoint);
@@ -33,67 +32,11 @@ public class SageInstantiate : MonoBehaviour
     // ロードステージ
     void RoadStage()
     {
-        if (clearpoint == 0)
+        if (clearpoint == clearpoint)
         {
             for (int count = 0; count < 1; count++)
             {
-                Instantiate(stageprefab[0]);
-            }
-        }
-
-        if (clearpoint == 1)
-        {
-            for (int count = 0; count < 1; count++)
-            {
-                Instantiate(stageprefab[1]);
-            }
-        }
-
-        if (clearpoint == 2)
-        {
-            for (int count = 0; count < 1; count++)
-            {
-                Instantiate(stageprefab[2]);
-            }
-        }
-
-        if (clearpoint == 3)
-        {
-            for (int count = 0; count < 1; count++)
-            {
-                Instantiate(stageprefab[3]);
-            }
-        }
-
-        if (clearpoint == 4)
-        {
-            for (int count = 0; count < 1; count++)
-            {
-                Instantiate(stageprefab[4]);
-            }
-        }
-
-        if (clearpoint == 5)
-        {
-            for (int count = 0; count < 1; count++)
-            {
-                Instantiate(stageprefab[5]);
-            }
-        }
-
-        if (clearpoint == 6)
-        {
-            for (int count = 0; count < 1; count++)
-            {
-                Instantiate(stageprefab[6]);
-            }
-        }
-
-        if (clearpoint == 7)
-        {
-            for (int count = 0; count < 1; count++)
-            {
-                Instantiate(stageprefab[7]);
+                Instantiate(stageprefab[clearpoint]);
             }
         }
     }
